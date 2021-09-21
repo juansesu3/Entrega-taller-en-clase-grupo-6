5dc27a110dfc41f5f30b70028901b8a555dd588d
juansesu3 <juanseu3@hotmail.com>
2021-09-20 20:02:00 -0500

    Solucion dificultad local storage

--- script/script.js
+++ script/script.js
@@ -31,35 +31,39 @@ function calcular(){
  }
 
 // tenemos  un error que dice:  form no esta definido por ende no capturamos los datos en el localstorage
-form.addEventListener('boton1', function AlmacenarLocalStorage(){
-    
+//form.addEventListener('salida',
+ 
+
+function AlmacenarLocalStorage(){
     //Captura de datos y almacenamiento en vairables
-    let cantidadcamisas1 = document.getElementById('cantidadCamisas').value;
-    let costocamisas1 = document.getElementById('costoCamisas').value;
-    let salida = document.getElementById('salida').value;
+    let cantidadcamisas1= document.getElementById('cantidadCamisas').value;
+    let presiocamisas1 = document.getElementById('costoCamisas').value;
+    //let gender = document.getElementById('gender').value;
     //let msm = document.getElementById('msm').value;
 
-    if(cantidadcamisas1==="" || costocamisas1 === "" || salida===""){
+    //|| gender==="" || msm ===""
+    if(cantidadcamisas1==="" || presiocamisas1=== ""){
          alert('Ingresar todos los campos');
     }else{
 
         localStorage.setItem('cantidadCamisas2',cantidadcamisas1);
-        localStorage.setItem('costoCamisas2',costocamisas1);
-        localStorage.setItem('salida2', salida);
+        localStorage.setItem('presioCamisa2',presiocamisas1);
+        //localStorage.setItem('Género',gender);
         //localStorage.setItem('Mensaje',msm);
         
         ObtenerLocalStorage();
     }
-})
+ }
+ //)
 
 function ObtenerLocalStorage(){
 
     let cantidadCamisas3 = localStorage.getItem('cantidadCamisas2');
-    let costoCamisas3 = localStorage.getItem('costoCamisas2');
-    let salida3 = localStorage.getItem('salida2');
+    let presioCamisas3 = localStorage.getItem('presioCamisa2');
+    //let genero = localStorage.getItem('Género');
     //let mensaje = localStorage.getItem('Mensaje');
     alert(`La información almacenada es 
-    ${cantidadCamisas3 }
-    ${costoCamisas3}
-    ${salida3}`);
+    ${cantidadCamisas3}
+    ${presioCamisas3}
+    `);
 }
